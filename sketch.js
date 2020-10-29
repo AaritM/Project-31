efa7f1f8b85d059d5c504ff3345e89b4b2f1a72f
AaritM <67886390+AaritM@users.noreply.github.com>
2020-10-28 19:03:57 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var ground ;
+
+function setup() {
+  createCanvas(480,800);
+  createSprite(400, 200, 50, 50);
+  ground = new Ground(width/2,780,width,10)
+  for (var j = 0; j < particles.length; j++) {
+
+    particles[j].push();
+  }
+
+  for (var k = 0; k < divisions.length, k++) {
+
+divisions[k].push();
+
+  }
+
+  for ( var j = 40; j<=width; j = j+50)
+  {
+
+    plinkos.push(new Plinko(j, 75));
+  }
+
+  for ( var j = 15; j<=width-10; j = j+50)
+  {
+
+    plinkos.push(new Plinko(j, 175));
+  }
+
+  for ( var j = 25; j<=width+10; j = j+50)
+  {
+
+    plinkos.push(new Plinko(j, 125));
+  }
+
+  for ( var j = 50; j<=width+5; j = j+50)
+  {
+
+    plinkos.push(new Plinko(j, 150));
+  }
+
+  if (framecount%60===0) {
+
+    particles.push(new Particle(random(width/2-10, width/2+10), 10, 10))
+  }
+
+}
+
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight=300
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  ground.display()
+}
+
+for (var k = 0; k = k + 80 ) {
+  divisions.push(new Divisions( k, height-divisionHeight/2, 10, divisionHeight))
+
+}
